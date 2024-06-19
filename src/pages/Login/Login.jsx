a69b50587e3a7760b2c41a8b8cc0e58e2ddf34b2
whis102 <chicongnguyen.dev@gmail.com>
2024-06-19 23:31:37 +0700

    update login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,38 @@
 import "./Login.scss";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+import Avatar from '@mui/material/Avatar';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+import AppleIcon from '@mui/icons-material/Apple';
 
 function Login() {
   return (
-    <div className="container">
-      <a href="">ABC</a>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+        <Container maxWidth="sm" className="bg">
+          <div className="favicon">
+            <img src="../src/assets/favicon.png" alt="" width={50} height={50}/>
+          </div>
+          <h3 className="title">Welcome back!</h3>
+          <span>
+            <p>Don't have an account ye?t</p>
+            <a href="">Register</a>
+          </span>
+
+          <form action="">
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+
+          <Button variant="contained">Login</Button>
+          </form>
+        </Container>
+    </React.Fragment>
+
+
   );
 }
 
